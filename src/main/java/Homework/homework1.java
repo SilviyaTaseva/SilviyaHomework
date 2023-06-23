fe0998f972e27c0b8c3ba7a6d41cb43509b3ff0b
SilviyaTaseva <silviya.r.taseva@gmail.com>
2023-06-23 22:24:44 +0300

    Initial commit

---
+++ src/main/java/Homework/homework1.java
@@ -0,0 +1,25 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class homework1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Silviya");
+
+
+        System.out.println("How do you spend your free time?");
+        Scanner scanner = new Scanner(System.in);
+
+        String hobby = scanner.nextLine();
+
+        System.out.println("Do you like to read through me too?");
+
+        Scanner scanner1 = new Scanner(System.in);
+        String answer = scanner.nextLine();
+
+        System.out.println("Nice :)");
+
+
+    }
+}
